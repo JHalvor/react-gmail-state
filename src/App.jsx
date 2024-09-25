b8bd0835ccfadd190bdf0035482b01ffb22eb66b
JHalvor <jonashalvorsen2013@live.no>
2024-09-25 18:22:17 +0200

    Add-star-read

--- src/App.jsx
+++ src/App.jsx
@@ -7,33 +7,53 @@ import './styles/App.css'
 
 function App() {
   const [emails, setEmails] = useState(initialEmails)
-  // const [inboxCounter, setInboxCounter] = useState(0)
-  // const [starredCounter, setStarredCounter] = useState(0)
-  // const [selected, setSelected] = useState([])
-  // Use initialEmails for state
+  const [hideRead, setHideRead] = useState(false)
+
+  const getStarredCount = () => {
+    return emails.reduce((count, email) => {
+      return email.starred ? count + 1 : count;
+    }, 0);
+  }
+
+  const getInboxCount = () => {
+    return emails.reduce((count, email) => {
+      return !email.read ? count + 1 : count;
+    }, 0);
+  }
 
   const updateStarred = (emailId) => {
-    setEmails((emails) => emails.find((email) => email.id === emailId).starred 
-                       = !emails.find((email) => email.id === emailId).starred)
+    setEmails((emails =>
+      emails.map((email) =>
+        email.id === emailId ? { ...email, starred: !email.starred } : email
+      )
+    ))
+  }
+
+  const updateRead = (emailId) => {
+    setEmails((emails =>
+      emails.map((email) =>
+        email.id === emailId ? { ...email, read: !email.read } : email
+      )
+    ))
   }
 
   const RenderEmail = (emailData) => {
-    //const email = document.createElement("li")
-    //email.className = "email"
-  
-    //email.appendChild(RenderSelect())
     return (
       <li className="email">
         <div className="select">
         <input
           className="select-checkbox"
-          type="checkbox"/>
+          type="checkbox"
+          onChange={() => updateRead(emailData.id)}
+          checked={emailData.read}
+        />
         </div>
         <div className="star">
         <input
           className="star-checkbox"
           type="checkbox"
-          onClick={() => updateStarred(emailData.id)}
+          onChange={() => updateStarred(emailData.id)}
+          checked={emailData.starred}
         />
         </div>
         <div className="sender">{emailData.sender}</div>
@@ -43,14 +63,22 @@ function App() {
   }
 
   const RenderEmails = (data) => {
-    const { id, sender, title } = data.emails[0]
     return (
-      <RenderEmail id={id} sender={sender} title={title}/>
+      <ul>
+        {
+          data.emails.map((email) => {
+            if (!hideRead || !email.read) {
+              return <RenderEmail key={email.id} id={email.id} sender={email.sender} title={email.title} starred={email.starred} read={email.read}/>
+            }
+            return <></>
+          })
+        }        
+      </ul>
     )
   }
 
-  console.log("emails")
-  console.log(emails)
+  // console.log("emails")
+  // console.log(emails)
 
   return (
     <div className="app">
@@ -62,14 +90,14 @@ function App() {
             // onClick={() => {}}
           >
             <span className="label">Inbox</span>
-            <span className="count">?</span>
+            <span className="count">{getInboxCount()}</span>
           </li>
           <li
             className="item"
             // onClick={() => {}}
           >
             <span className="label">Starred</span>
-            <span className="count">?</span>
+            <span className="count">{getStarredCount()}</span>
           </li>
 
           <li className="item toggle">
@@ -77,8 +105,8 @@ function App() {
             <input
               id="hide-read"
               type="checkbox"
-              checked={false}
-              // onChange={() => {}}
+              checked={hideRead}
+              onChange={() => {setHideRead(!hideRead)}}
             />
           </li>
         </ul>
